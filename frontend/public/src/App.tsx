306674727d465784fc37f5e1dc44b6c21ea243f2
mehdihassantoori <adnanhaider9993@gmail.com>
2025-03-19 11:37:53 +0500

    Update App.tsx

--- frontend/public/src/App.tsx
+++ frontend/public/src/App.tsx
@@ -1,25 +1,26 @@
 // @ts-nocheck
 
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard'; // Import Dashboard component
-import SignUp from './pages/SignUp'; // Import SignUp component
-import ProtectedRoute from './ProtectedRoute';
-import NotFound from './pages/NotFound'; // Assuming you have a NotFound component
-import Profile from './components/profile'; // Import Profile component
-import Posts from './components/Posts'; // Import Posts component
-import Editor from './components/Editor'; // Import Editor component
-import ImageEditor from './components/ImageEditor'; // Import ImageEditor component
-import FullEditor from './components/ImageEditor/FullEditor'; // Import FullEditor component
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard"; // Import Dashboard component
+import SignUp from "./pages/SignUp"; // Import SignUp component
+import ProtectedRoute from "./ProtectedRoute";
+import NotFound from "./pages/NotFound"; // Assuming you have a NotFound component
+import Profile from "./components/profile"; // Import Profile component
+import Posts from "./components/Posts"; // Import Posts component
+import Editor from "./components/Editor"; // Import Editor component
+import ImageEditor from "./components/ImageEditor"; // Import ImageEditor component
+import FullEditor from "./components/ImageEditor/FullEditor"; // Import FullEditor component
 function App() {
+  // yfytdfty
   return (
     <Router>
       <Routes>
         {/* The "landing" route */}
         <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home/>}/>
+        <Route path="/home" element={<Home />} />
         {/* If user not authenticated, they can sign up or log in */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
@@ -38,6 +39,5 @@ function App() {
     </Router>
   );
 }
-
 
 export default App;
